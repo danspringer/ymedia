864f572727d28a21741c7a4e0f48a0dd0d15bb93
Daniel Springer <daniel.springer@medienfeuer.de>
2024-02-29 12:28:10 +0100

    Update install.php

--- install.php
+++ install.php
@@ -2,19 +2,17 @@
 
 rex_yform_manager_table::deleteCache();
 
-$content = rex_file::get(rex_path::addon('ycom', 'install/tablesets/ymedia.json'));
+$content = rex_file::get(rex_path::addon('ymedia', 'install/tablesets/ymedia.json'));
 if (is_string($content) && '' != $content) {
     rex_yform_manager_table_api::importTablesets($content);
 }
 
-$content = rex_file::get(rex_path::addon('ycom', 'install/tablesets/ymedia_tag.json'));
+$content = rex_file::get(rex_path::addon('ymedia', 'install/tablesets/ymedia_tag.json'));
 if (is_string($content) && '' != $content) {
     rex_yform_manager_table_api::importTablesets($content);
 }
 
-$content = rex_file::get(rex_path::addon('ycom', 'install/tablesets/ymedia_category.json'));
+$content = rex_file::get(rex_path::addon('ymedia', 'install/tablesets/ymedia_category.json'));
 if (is_string($content) && '' != $content) {
     rex_yform_manager_table_api::importTablesets($content);
 }
-
-
